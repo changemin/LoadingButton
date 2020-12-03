6c8b21cdf707131d8a75ac20770b2598f92ccd8c
Changemin <bkm.change.min@gmail.com>
2020-12-03 18:18:38 +0900

    Implement 'custom functions'

--- Sources/CMLoadingButton/CMLoadingButton.swift
+++ Sources/CMLoadingButton/CMLoadingButton.swift
@@ -1,75 +1,73 @@
 import SwiftUI
 
+public func endLoading(with: EndAnimation) -> Void {
+    withAnimation {
+        //isLoading = false
+        // shake or not
+    }
+}
+
 public struct CMLoadingButton<Content: View>: View{
     @Binding var isLoading: Bool
     var style: CMButtonStyle? = CMButtonStyle()
     let content: Content
-    var action: Void
     var endAnimation: EndAnimation = .normal
+    var action: () -> () = {}
     
     public init(action: @escaping () -> Void, isLoading: Binding<Bool>, endAnimation: EndAnimation, style: CMButtonStyle, @ViewBuilder builder: () -> Content) {
         self._isLoading = isLoading
         self.style = style
         self.endAnimation = endAnimation
         content = builder()
-//        print(type(of: action))
+        print(type(of: action))
     }
     
     public init(action: @escaping () -> Void, isLoading: Binding<Bool>, @ViewBuilder builder: () -> Content) {
-        
         self._isLoading = isLoading
         content = builder()
-//        print(type(of: action))
+        self.action = action
     }
     
     public var body: some View {
-            Button(action: {
-                withAnimation(.easeInOut) {
-                    isLoading = true
+        Button(action: {
+            withAnimation(.easeInOut) {
+                isLoading = true
+            }
+            action()
+        }) {
+            ZStack {
+                Rectangle()
+                    .fill(isLoading ? style!.loadingBackgroundColor : style!.backgroundColor)
+                    .frame(width: isLoading ? style!.height : style!.width, height: style!.height)
+                    .cornerRadius(isLoading ? style!.height/2 : style!.cornerRadius)
+                if isLoading {
+                    if #available(iOS 14.0, *) {
+                        ProgressView()
+                    }
+                }
+                else {
+                    VStack {
+                        content
+                        
+                    }
                 }
                 
-                // custom actions here
-            }) {
-                ZStack {
-                    Rectangle()
-                        .fill(isLoading ? style!.loadingBackgroundColor : style!.backgroundColor)
-                        .frame(width: isLoading ? style!.height : style!.width, height: style!.height)
-                        .cornerRadius(isLoading ? style!.height/2 : style!.cornerRadius)
-                    if isLoading {
-                        if #available(iOS 14.0, *) {
-                            ProgressView()
-                        }
-                    }
-                    else {
-                        VStack {
-                            content
-                            
-                        }
-                    }
-                    
-                }
-                
-            }.frame(width: style!.width, height: style!.height)
+            }
+        }.frame(width: style!.width, height: style!.height)
     }
-    public func ifFail(_ endAnimation: EndAnimation) {
-        if(endAnimation == .normal) {
-            // reverse to normal size
-        }
-        else if (endAnimation == .shake) {
-            // reverse to normal size and shake the button
-        }
-    }
+    
+    /// Start Loading
     public func startLoading() -> Void {
         withAnimation() {
             self.isLoading = true
         }
     }
-    public func endLoading() -> Void {
-        self.isLoading = false
-    }
-    public func toggle() -> Void {
-        self.isLoading.toggle()
-    }
     
+    /// End Loading with EndAnimation(normal, shake, expand)
+    public func endLoading(with: EndAnimation) -> Void {
+        withAnimation {
+            self.isLoading = false
+        }
+    }
 }
 
