6d3c03ec41a17d2d16bd8353628a87df519f1ebf
Changemin <bkm.change.min@gmail.com>
2020-12-03 12:46:50 +0900

    Send 'Actions' as 'Parameter'

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # CMLoadingButton
 
+## Features
+* Pure Swift 5
+* Fully Configuarable
+
 ## Installaion
     File ➜ Swift Packages ➜ Add Package Dependancy..
 
--- Sources/CMLoadingButton/CMLoadingButton.swift
+++ Sources/CMLoadingButton/CMLoadingButton.swift
@@ -4,9 +4,17 @@ public struct CMButtonStyle {
     public init() {
         
     }
+    public init(w width: CGFloat, h height: CGFloat, r cornerRadius: CGFloat, bgColor: Color, subColor: Color, animation: Animation) {
+        self.width = width
+        self.height = height
+        self.cornerRadius = cornerRadius
+        self.backgroundColor = bgColor
+        self.loadingBackgroundColor = subColor
+        
+    }
     public var width: CGFloat = 312
     public var height: CGFloat = 54
-    public var cornerRadius: CGFloat = 27
+    public var cornerRadius: CGFloat = 0
     public var backgroundColor: Color = .green
     public var loadingBackgroundColor: Color = Color.green.opacity(0.7)
     public var animation: Animation = .easeInOut
@@ -19,24 +27,37 @@ public enum EndAnimation {
 
 public struct CMLoadingButton<Content: View>: View{
     @Binding var isLoading: Bool
-    var style: CMButtonStyle
+    var style: CMButtonStyle? = CMButtonStyle()
     let content: Content
+    var action: Void
     
-    public init(isLoading: Binding<Bool>, style: CMButtonStyle, @ViewBuilder builder: () -> Content) {
+    public init(action: @escaping () -> Void, isLoading: Binding<Bool>, style: CMButtonStyle, @ViewBuilder builder: () -> Content) {
         self._isLoading = isLoading
         self.style = style
         content = builder()
+        print(type(of: action))
+    }
+    
+    public init(action: @escaping () -> Void, isLoading: Binding<Bool>, @ViewBuilder builder: () -> Content) {
+        self._isLoading = isLoading
+        content = builder()
+        print(type(of: action))
     }
     
     public var body: some View {
             Button(action: {
+                withAnimation(.easeInOut) {
+                    isLoading = true
+                    
+                }
                 
+                // custom actions here
             }) {
                 ZStack {
                     Rectangle()
-                        .fill(isLoading ? style.loadingBackgroundColor : style.backgroundColor)
-                        .frame(width: isLoading ? style.height : style.width, height: style.height)
-                        .cornerRadius(isLoading ? style.height/2 : style.cornerRadius)
+                        .fill(isLoading ? style!.loadingBackgroundColor : style!.backgroundColor)
+                        .frame(width: isLoading ? style!.height : style!.width, height: style!.height)
+                        .cornerRadius(isLoading ? style!.height/2 : style!.cornerRadius)
                     if isLoading {
                         if #available(iOS 14.0, *) {
                             ProgressView()
@@ -51,7 +72,7 @@ public struct CMLoadingButton<Content: View>: View{
                     
                 }
                 
-            }.frame(width: style.width, height: style.height)
+            }.frame(width: style!.width, height: style!.height)
     }
     public func startLoading() -> Void {
         withAnimation() {
@@ -65,3 +86,4 @@ public struct CMLoadingButton<Content: View>: View{
         self.isLoading.toggle()
     }
 }
+
